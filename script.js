d55cc74de36f4a6e1e5b8fe0e4d595bb0a00c73b
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-12-15 17:31:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+
+var landscapeImage = "https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg";
+var portraitImage = "https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg";
+
+
+window.addEventListener("resize", changeBackgroundImage);
+
+function changeBackgroundImage() {
+    if (window.innerHeight > window.innerWidth) {
+       
+        document.body.style.backgroundImage = "url('" + portraitImage + "')";
+    } else {
+       
+        document.body.style.backgroundImage = "url('" + landscapeImage + "')";
+    }
+}
+
+
+changeBackgroundImage();
+
+window.addEventListener("resize", changeBackgroundImage);
